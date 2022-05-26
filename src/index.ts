39db91c73bee2499b322fec395f47b3a4cdfb872
waelebrahim65@gmail.com <waelebrahim65@gmail.com>
2022-05-26 22:08:58 +0200

    build server and fix some issue in eslint and nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello')
+import express from 'express';
+
+// create instant server
+const app = express();
+
+const port = 3000;
+
+// add routing for / path
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World 🌍',
+  })
+})
+
+
+// start express server
+app.listen(port, ()=> {
+    console.log(`server started at localhost:${port}`)
+    });
+    
+
+export default app
